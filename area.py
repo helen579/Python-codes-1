8fd2137a35f4e589a69ce651f5b8a7fd4c5db259
Helen mary varghese <65602897+helen579@users.noreply.github.com>
2021-05-28 22:41:20 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,5 @@
+import math
+rad = float(input("Input the radius of the circle: "))
+area = math.pi * rad*rad
+print("The area of the circle with radius %.1f is: " %rad)
+print("%.16f" %area)
---
+++ filename.py
@@ -0,0 +1,3 @@
+filename = (input("Input the filename: "))
+extn = filename.split(".");
+print("The extension of the file is: " +repr(extn[-1]));
